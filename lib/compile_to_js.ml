5611692eb175df48e56d4876d847bc37476d004d
Andrea Cognolato <andrecogno@hotmail.it>
2018-09-01 17:34:49 +0200

    Add command line args

--- lib/compile_to_js.ml
+++ lib/compile_to_js.ml
@@ -1,30 +1,33 @@
 module P = Parse
 
-let rec compile (ast: P.ast): string =
-  match ast with
-  | P.Digit d -> string_of_int d
-  | P.Variable label -> label
-  | P.Expr (e1, op, e2) -> (
-      let string_of_op = function
-        | P.Sum -> "+"
-        | P.Subtraction -> "-"
-        | P.Multiplication -> "*"
-        | P.Division -> "/"
-        | P.GreaterThan -> ">"
-        | P.LesserThan -> "<"
-      in
-      (compile e1) ^ (string_of_op op) ^ (compile e2)
+let compile ast =
+  let rec compile (ast: P.ast): string =
+    match ast with
+    | P.Digit d -> string_of_int d
+    | P.Variable label -> label
+    | P.Expr (e1, op, e2) -> (
+        let string_of_op = function
+          | P.Sum -> "+"
+          | P.Subtraction -> "-"
+          | P.Multiplication -> "*"
+          | P.Division -> "/"
+          | P.GreaterThan -> ">"
+          | P.LesserThan -> "<"
+        in
+        (compile e1) ^ (string_of_op op) ^ (compile e2)
+      )
+    | P.LetExpr (label, e1, e2) -> (
+        (* FIXME: this doesn't work  *)
+        "{" ^ "const " ^ label ^ " = " ^ (compile e1) ^ "; return (" ^ (compile e2) ^ ")}"
+      )
+    | P.Parenthesised e1 -> "(" ^ (compile e1) ^ ")"
+    | P.IfElseExpr(e1,e2,e3) -> (
+        (compile e1) ^ " ? " ^ (compile e2) ^ " : " ^ (compile e3)
+      )
+    | P.FunDecl (label, _type, e1) -> (
+        label ^ " => " ^ (compile e1)
+      )
+    | P.FunApp (e1, e2) -> (
+        (compile e1) ^ "(" ^ (compile e2) ^ ")"
     )
-  | P.LetExpr (label, e1, e2) -> (
-      "{" ^ "const " ^ label ^ " = " ^ (compile e1) ^ "; return " ^ (compile e2) ^ "}"
-    )
-  | P.Parenthesised e1 -> "(" ^ (compile e1) ^ ")"
-  | P.IfElseExpr(e1,e2,e3) -> (
-      (compile e1) ^ " ? " ^ (compile e2) ^ " : " ^ (compile e3)
-    )
-  | P.FunDecl (label, _type, e1) -> (
-      label ^ " => " ^ (compile e1)
-    )
-  | P.FunApp (e1, e2) -> (
-      (compile e1) ^ "(" ^ (compile e2) ^ ")"
-    )
+  in "console.log((function() {" ^ compile ast ^ "})())"
--- repl.ml
+++ repl.ml
@@ -11,17 +11,36 @@ let rec loop () =
         |> Lex.lex
         |> Parse.parse
       in
-      let _expr_type = Typecheck.typecheck ast in
+      let expr_type = Typecheck.typecheck ast in
       ast
-      (* |> (fun x -> Eval.eval x [])
+      |> (fun x -> Eval.eval x [])
          |> Eval.show_expr_type
-         |> printf "%s :: %s\n" (Typecheck.show_expr_type expr_type); *)
-      |> Compile_to_js.compile
-      |> printf "%s\n";
+         |> printf "%s :: %s\n" (Typecheck.show_expr_type expr_type);
+      (* |> Compile_to_js.compile
+      |> printf "%s\n"; *)
       loop ()
     )
   | None ->
     print_endline "Goodbye";
     ()
 
-let () = loop ();
+let () =
+  let speclist = [
+    ("-r", Arg.Unit loop, "Opens the repl");
+    ("-c", Arg.Unit (fun () -> (
+      let program = In_channel.(input_all stdin) in
+      let ast =
+        program
+        |> Lex.lex
+        |> Parse.parse
+      in
+      Typecheck.typecheck ast |> ignore;
+      ast
+      |> Compile_to_js.compile
+      |> printf "%s\n";
+    )), "Compiles the input to Javascript");
+    ] in
+  let usage_msg = "The test-lang interpreter and JS compiler." in
+  let anonymous_arg_fun =
+    fun _ -> () in
+  Arg.parse speclist anonymous_arg_fun usage_msg;
